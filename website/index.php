b53a1ab74ef48b72d3c218716aeef4eb828bc055
woswos <barkin.simsek@hotmail.com>
2019-03-30 14:14:33 -0700

    added html

--- website/index.php
+++ website/index.php
@@ -1,3 +1,16 @@
 <?php
 echo "test";
 ?>
+
+
+<html>
+  <head>
+    <title>barkin | viusal cryptography</title>
+    <meta charset="utf-8" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+  </head>
+  <body>
+
+  </body>
+</html>
